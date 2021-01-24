ebb65f43eac6c27064c71437789ffa44b8f42181
followgodstudio <followgod.studio@gmail.com>
2021-01-24 17:36:15 -0600

    put bandage for development

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -59,6 +59,7 @@ class Auth extends React.Component {
                 <Row className="justify-content-center">
                   <Col lg="5" md="6">
                     <h1 className="text-white">Welcome!</h1>
+                    <h2>This is Follow Him Development Environment</h2>
                     {/*<p className="text-lead text-light">*/}
                     {/*  Use these awesome forms to login or create new account in*/}
                     {/*  your project for free.*/}
